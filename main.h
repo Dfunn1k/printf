052fbbda550c7feed1c3d70199aa1312f18c7ba1
Mcfreestyle <mcfreestyle.go1@gmail.com>
2021-11-08 09:03:17 -0800

    fix: change the return value of the function prototypes to int

--- main.h
+++ main.h
@@ -15,15 +15,15 @@
 typedef struct op
 {
 	char *op;
-	void (*f)(va_list list);
+	int (*f)(va_list list);
 } op_t;
 
 int _printf(const char *format, ...);
-void (*match(const char *s))(va_list list);
-void op_char(va_list list);
-void op_string(va_list list);
-void op_integer(va_list list);
-void op_binary(va_list list);
+int (*match(const char *s))(va_list list);
+int op_char(va_list list);
+int op_string(va_list list);
+int op_integer(va_list list);
+int op_binary(va_list list);
 int _strlen(char *s);
 void rev_string(char *s);
 
