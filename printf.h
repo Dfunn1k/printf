f28016a7f618bfae7a99464c2f7ef7e98c529400
Mcfreestyle <mcfreestyle.go1@gmail.com>
2021-11-07 20:50:23 -0800

    ci: add function prototypes and header files

--- printf.h
+++ printf.h
@@ -2,6 +2,9 @@
 #define _PRINTF_
 
 #include <stdarg.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stddef.h>
 
 /**
  * struct op - Struct op
@@ -19,5 +22,8 @@ int _printf(const char *format, ...);
 void (*match(const char *s))(va_list list);
 void op_char(va_list list);
 void op_string(va_list list);
+void op_integer(va_list list);
+int _strlen(char *s);
+void rev_string(char *s);
 
 #endif
