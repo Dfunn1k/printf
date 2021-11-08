814dc3572d82037fda674588851bfda7eb17d5b1
Mcfreestyle <mcfreestyle.go1@gmail.com>
2021-11-08 09:01:11 -0800

    fix: change the return value of the functions to int

--- function_types.c
+++ function_types.c
@@ -1,24 +1,28 @@
 #include "main.h"
 
 /**
- *op_char - function that display char
- *@list: argument passed
+ * op_char - function that display char
+ * @list: argument passed
+ *
+ * Return: number of printed characters
  */
-void op_char(va_list list)
+int op_char(va_list list)
 {
 	char ptr = va_arg(list, int);
 
-	write(1, &ptr, 1);
+	return (write(1, &ptr, 1));
 }
 
 /**
- *op_string - function that display a string
- *@list: argument passed
+ * op_string - function that display a string
+ * @list: argument passed
+ *
+ * Return: number of printed characters
  */
-void op_string(va_list list)
+int op_string(va_list list)
 {
 	char *str;
 
 	str = va_arg(list, char *);
-	write(1, str, _strlen(str));
+	return (write(1, str, _strlen(str)));
 }
