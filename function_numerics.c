09dcb239f091f80a9f63f4423a4b8b2bbefab919
BigDany1792 <dany1792dota@gmail.com>
2021-11-08 09:00:07 -0500

    feat: added function binary

--- function_numerics.c
+++ function_numerics.c
@@ -33,3 +33,37 @@ void op_integer(va_list list)
 	write(1, str, _strlen(str));
 	free(str);
 }
+
+/**
+ *
+ *
+ *
+ */
+void op_binary(va_list list)
+{
+	int num, temp, i = 0, count = 0;
+	char *str;
+	num = va_arg(list, int);
+	temp = num;
+
+	while(num != 0)
+	{
+		num = num / 2;
+		count++;
+	}
+	num = temp;
+
+	str = malloc(sizeof(char) * (count + i));
+	if (str == NULL)
+		return;
+	while(num != 0)
+	{
+		str[i] = (num % 2) + 48;
+		num = num / 2:
+		i++;
+	}
+	str[count] = '\0';
+	rev_string(str);
+	write(1, str, _strlen(str));
+	free(str);
+}
